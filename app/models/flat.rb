51224edffd99da1dc75a3ea25eed57d786ab7661
Usman Ali <usmanv5@hotmail.com>
2023-11-22 14:42:16 +0000

    added new flat functionality and some error handling

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,6 @@
 class Flat < ApplicationRecord
+  validates :name, :address, presence: true
+  validates :description, presence: true
+  validates :price_per_night, :number_of_guests, numericality: { only_integer: true }
+  validates :price_per_night, :number_of_guests, presence: true
 end
